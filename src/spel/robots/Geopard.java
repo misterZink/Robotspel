825dbcdd901237179c7b5641082d333b8ec8007f
Hojsan69 <71629555+Hojsan69@users.noreply.github.com>
2020-09-22 11:20:26 +0200

    Geopard och zebra
    Har påbörjat prjektet med zebra och Geopard

--- src/spel/robots/Geopard.java
+++ src/spel/robots/Geopard.java
@@ -2,4 +2,42 @@ package spel.robots;
 
 public class Geopard extends Robot {
 
+    private boolean isHungry;
+    private boolean isTired;
+
+    private int antalVila;
+
+    public Geopard() {
+        isHungry = true;
+        isTired = false;
+        antalVila = 0;
+
+    }
+    public void eatZebra(Zebra z) {
+        z.setDead(true);
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
+    public void setHungry(boolean hungry) {
+        isHungry = hungry;
+    }
+
+    public boolean isTired() {
+        return isTired;
+    }
+
+    public void setTired(boolean tired) {
+        isTired = tired;
+    }
+
+    public int getAntalVila() {
+        return antalVila;
+    }
+
+    public void setAntalVila(int antalVila) {
+        this.antalVila = antalVila;
+    }
 }
--- src/spel/robots/Zebra.java
+++ src/spel/robots/Zebra.java
@@ -1,5 +1,20 @@
 package spel.robots;
 
 public class Zebra extends Robot {
+    private Boolean isDead;
 
+
+    //deafult konstruktor, Zebran när man skapar objektet
+    public Zebra() {
+        isDead=false;
+    }
+    // getter returnera isdead till den som anropar metoden.
+    public boolean isDead() {
+        return isDead;
+    }
+    //setters sätter värdet på is dead
+    public void setDead(boolean dead) {
+        isDead=dead;
+    }
 }
+
