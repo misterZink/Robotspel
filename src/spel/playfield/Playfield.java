960b5f221965c4cc56bee5814f7f2cb4c46534bc
Christian Pilz <svampodling@gmail.com>
2020-09-23 10:54:35 +0200

    We can now print the playfield with different Robots spawned randomly.

--- src/spel/playfield/Playfield.java
+++ src/spel/playfield/Playfield.java
@@ -9,6 +9,8 @@ import java.util.Random;
 public class Playfield {
 
     private Robot[][] robots;
+    final int spawnIntLimit = 100;
+    final int spawnChance = 7;
 
     public Robot[][] getRobots() {
         return robots;
@@ -23,43 +25,52 @@ public class Playfield {
 
     public void spawnGeopards(final int amountOfGeopards) {
         int geopardsSpawned = 0;
+        boolean allGeopardsSpawned = false;
         Random random = new Random();
 
-        for (int j = 0; j < robots.length; j++) {
-            for (int k = 0; k < robots[j].length; k++) {
+        while (!allGeopardsSpawned) {
+            for (int j = 0; j < robots.length; j++) {
+                for (int k = 0; k < robots[j].length; k++) {
 
-                if (geopardsSpawned < amountOfGeopards) {
-                    if (robots[j][k] == null) {
-                        final boolean toSpawn = random.nextBoolean();
+                    if (geopardsSpawned < amountOfGeopards) {
+                        if (robots[j][k] == null) {
+                            final int toSpawn = random.nextInt(spawnIntLimit);
 
-                        if (toSpawn) {
-                            robots[j][k] = new Geopard();
-                            geopardsSpawned++;
-                            System.out.println("Geopard " + geopardsSpawned + "/" + amountOfGeopards + " spawned @" + " [" + j + "][" + k + "]");
+                            if (toSpawn < spawnChance) {
+                                robots[j][k] = new Geopard();
+                                geopardsSpawned++;
+                                System.out.println("Geopard " + geopardsSpawned + "/" + amountOfGeopards + " spawned @" + " [" + j + "][" + k + "]");
+                            }
                         }
+                    } else {
+                        allGeopardsSpawned = true;
                     }
                 }
             }
         }
-
     }
 
     public void spawnZebras(final int amountOfZebras) {
         int zebrasSpawned = 0;
+        boolean allZebrasSpawned = false;
         Random random = new Random();
 
-        for (int j = 0; j < robots.length; j++) {
-            for (int k = 0; k < robots[j].length; k++) {
+        while (!allZebrasSpawned) {
+            for (int j = 0; j < robots.length; j++) {
+                for (int k = 0; k < robots[j].length; k++) {
 
-                if (zebrasSpawned < amountOfZebras) {
-                    if (robots[j][k] == null) {
-                        final boolean toSpawn = random.nextBoolean();
+                    if (zebrasSpawned < amountOfZebras) {
+                        if (robots[j][k] == null) {
+                            final int toSpawn = random.nextInt(spawnIntLimit);
 
-                        if (toSpawn) {
-                            robots[j][k] = new Zebra();
-                            zebrasSpawned++;
-                            System.out.println("Zebra " + zebrasSpawned + "/" + amountOfZebras + " spawned @" + " [" + j + "][" + k + "]");
+                            if (toSpawn < spawnChance) {
+                                robots[j][k] = new Zebra();
+                                zebrasSpawned++;
+                                System.out.println("Zebra " + zebrasSpawned + "/" + amountOfZebras + " spawned @" + " [" + j + "][" + k + "]");
+                            }
                         }
+                    } else {
+                        allZebrasSpawned = true;
                     }
                 }
             }
